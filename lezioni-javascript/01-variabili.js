49d81c9d11e750ad9156a55f911bc38d7983f914
CarloDM <carlo.de.mauro.work@gmail.com>
2026-03-30 09:43:36 +0200

    js variabili

---
+++ lezioni-javascript/01-variabili.js
@@ -0,0 +1,17 @@
+console.log('ciao');
+
+function saluta(params) {
+  console.log(params);
+};
+
+saluta('ciao sono carlo');
+console.log(console);
+let
+myName = 'carlo'
+eta = 36
+// template literal 
+template = `ciao mi chiamo ${myName} e ho ${eta} anni`;
+
+console.log(template);
+console.log(template.length);
+
